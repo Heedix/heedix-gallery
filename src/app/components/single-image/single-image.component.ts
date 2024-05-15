8b227cb82118d3137a156e11735718d5267dc50b
JannisA <jannis.austgen@gmail.com>
2024-05-15 19:48:42 +0200

    - modified single-image-component including functionality

--- src/app/components/single-image/single-image.component.ts
+++ src/app/components/single-image/single-image.component.ts
@@ -6,6 +6,7 @@ import {ActivatedRoute} from "@angular/router";
 import {ImageService} from "../../services/image.service";
 import * as ExifReader from 'exifreader';
 import {readFile} from "node:fs";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-single-image',
@@ -25,7 +26,7 @@ export class SingleImageComponent implements OnInit{
 
   imageInterfaces: ImageInterface[] = [];
 
-  constructor(private imageService:ImageService) {}
+  constructor(private imageService:ImageService, private router: Router) {};
 
   async ngOnInit() {
     const imageId = Number(this.route.snapshot.paramMap.get('id'))
@@ -41,17 +42,24 @@ export class SingleImageComponent implements OnInit{
         visible: item.visible
       }));
       console.log(this.imageInterfaces)
-
+      if(this.imageInterfaces[0] == null) {
+        this.router.navigate(['']) //TODO ** Rout einfügen
+      }
     })
 
     //const file = this.imageInterfaces[0].source;
-    const tags = await ExifReader.load('../assets/images/bild2.jpg');
+    /*const tags = await ExifReader.load('../assets/images/bild2.jpg');
     // @ts-ignore
     const imageDate = tags['DateTimeOriginal'].description;
     // @ts-ignore
     const unprocessedTagValue = tags['DateTimeOriginal'].value;
 
-    console.log(imageDate + unprocessedTagValue + 'fdsfsd')
+    console.log(imageDate + unprocessedTagValue + 'fdsfsd');*/
 
+  }
+
+  copyLink = () => {
+    let link = "http://heedix.de/image/" + this.imageInterfaces[0].imageId;
+    navigator.clipboard.writeText(link).then(r => r);
   }
 }
