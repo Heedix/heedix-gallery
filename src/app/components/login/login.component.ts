a9b48941ae4080679b249dd513c7bf3bbc62ded7
Jannis Austgen <jannis.austgen@gmail.com>
2024-10-18 12:34:10 +0200

    - Modified login page - Modified single image view - Changed general background color

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,28 +2,40 @@ import { Component } from '@angular/core';
 import {FormsModule} from "@angular/forms";
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import {SidebarComponent} from "../sidebar/sidebar.component";
+import {NgClass, NgIf} from "@angular/common";
+import {repeat} from "rxjs";
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    SidebarComponent,
+    NgClass,
+    NgIf
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 
 export class LoginComponent {
+  isRegisterShown = false;
+
   username: string = '';
   password: string = '';
 
   constructor(private authService: AuthService, private router: Router) {}
 
+  toggleRegisterShown(): void {
+    this.isRegisterShown = !this.isRegisterShown;
+  }
+
   onSubmit() {
     this.authService.login(this.username, this.password).subscribe(
       (response) => {
         // Erfolgreiches Login
-        this.router.navigate(['/dashboard']);  // Navigiere zu einer sicheren Seite
+        this.router.navigate(['/']);  // Navigiere zu einer sicheren Seite
       },
       (error) => {
         // Fehler beim Login
@@ -31,4 +43,6 @@ export class LoginComponent {
       }
     );
   }
+
+  protected readonly repeat = repeat;
 }
