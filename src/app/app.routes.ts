d09203831c2eeaff4402bf55fec87eaef4d9ecf0
jannis <jannis.austgen@gmail.com>
2024-12-04 10:31:32 +0100

    - renamed account-content-view to dashboard - added dashboard icon - modified login icon

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import {HomeComponent} from "./components/home/home.component";
 import {SingleImageComponent} from "./components/single-image/single-image.component";
 import {NotFoundComponent} from "./components/not-found/not-found.component";
 import {LoginComponent} from "./components/login/login.component";
-import {AccountContentViewComponent} from "./components/account-content-view/account-content-view.component";
+import {DashboardComponent} from "./components/dashboard/dashboard.component";
 import {VerifyEmailComponent} from "./components/verify-email/verify-email.component";
 
 export const routes: Routes = [
@@ -12,7 +12,7 @@ export const routes: Routes = [
   {path: 'settings', component: SettingsComponent},
   {path: 'login', component: LoginComponent},
   {path: 'verify', component: VerifyEmailComponent},
-  {path: 'account', component: AccountContentViewComponent},
+  {path: 'dashboard', component: DashboardComponent},
   {path: 'image/:id', component: SingleImageComponent},
   {path: '**', redirectTo: '404'},
   {path: '404', component: NotFoundComponent}
