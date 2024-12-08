9e1e987e27d8f452a4cae475697a95659933e190
Jannis Austgen <jannis.austgen@gmail.com>
2024-12-08 16:58:31 +0100

    - added environment variables

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,8 +2,9 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import CryptoJS from 'crypto-js';
+import {environment} from '../environments/environment';
 
-const API_URL = 'http://localhost:3000/api';
+const API_URL = environment.apiUrl;
 
 @Injectable({
   providedIn: 'root'
