813e53b887d6f1f4ea9c222a19123d6665d7d54c
Jannis Austgen <jannis.austgen@gmail.com>
2024-12-09 23:38:47 +0100

    - added drag and drop to add-image-component

--- src/app/components/dashboard/add-image/add-image.component.ts
+++ src/app/components/dashboard/add-image/add-image.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import {NgOptimizedImage} from "@angular/common";
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {NgIf, NgOptimizedImage} from "@angular/common";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
@@ -8,12 +8,16 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
   imports: [
     NgOptimizedImage,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    NgIf
   ],
   templateUrl: './add-image.component.html',
   styleUrl: './add-image.component.css'
 })
 export class AddImageComponent {
+  @ViewChild('fileInput') fileInput!: ElementRef<HTMLInputElement>;
+  selectedFile: File | null = null;
+
   name = '';
   visibility = 'not-listed';
   width = 0;
@@ -32,6 +36,51 @@ export class AddImageComponent {
   focalLength = '';
   whiteBalance = '';
   focalLengthEquivalent = '';
+  previewSrc: string | ArrayBuffer | null | undefined = null;
 
+  draggedOver = false;
+
+  onDragOver(event: DragEvent) {
+    event.preventDefault();
+    this.draggedOver = true;
+  }
+
+  onDragLeave() {
+    this.draggedOver = false;
+  }
+
+  onDrop(event: DragEvent) {
+    event.preventDefault();
+    this.draggedOver = false;
+
+    if (event.dataTransfer?.files && event.dataTransfer.files.length > 0) {
+      this.processFile(event.dataTransfer.files[0]);
+    }
+  }
+
+  triggerFileInput() {
+    this.fileInput.nativeElement.click(); // Öffne den Dateiauswahl-Dialog
+  }
+
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      this.processFile(input.files[0]);
+    }
+  }
+
+  processFile(file: File) {
+    if (file.type.startsWith('image/')) {
+      this.selectedFile = file;
+
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        this.previewSrc = e.target?.result; // Vorschau setzen
+      };
+      reader.readAsDataURL(file);
+    } else {
+      alert('Bitte nur Bilddateien auswählen.');
+    }
+  }
 
 }
